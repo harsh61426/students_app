f87ecd815c4e769c538f445b23b866daa4cbff09
Sarath <sarathchandra1999@gmail.com>
2017-09-26 23:19:37 +0530

    student search UI updated

---
+++ app/src/main/java/in/ac/iitm/students/objects/StudentSearchAccomDetails.java
@@ -0,0 +1,10 @@
+package in.ac.iitm.students.objects;
+
+/**
+ * Created by sarath on 26/09/17.
+ */
+
+public class StudentSearchAccomDetails {
+    public String accomOrgan,accomPos,accomFromyear,accomToyear;
+    public int no;
+}
