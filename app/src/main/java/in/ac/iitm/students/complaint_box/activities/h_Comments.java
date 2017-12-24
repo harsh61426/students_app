25a8d4bbe03051703ab8fc543bd670a8957f06b0
harsh61426 <s.harshitha016@gmail.com>
2017-12-24 20:56:25 +0530

    general complaints working

--- app/src/main/java/in/ac/iitm/students/complaint_box/activities/h_Comments.java
+++ app/src/main/java/in/ac/iitm/students/complaint_box/activities/h_Comments.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -76,6 +77,7 @@ public class h_Comments extends AppCompatActivity {
         StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                Log.e("h",response);
 
                 h_CmntDataParser hCmntDataParser = new h_CmntDataParser(response, getApplicationContext());
                 ArrayList<h_CommentObj> commentArray = null;
--- app/src/main/java/in/ac/iitm/students/complaint_box/adapters/g_ComplaintAdapter.java
+++ app/src/main/java/in/ac/iitm/students/complaint_box/adapters/g_ComplaintAdapter.java
@@ -32,6 +32,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import in.ac.iitm.students.R;
+import in.ac.iitm.students.complaint_box.activities.g_Comments;
 import in.ac.iitm.students.complaint_box.activities.h_Comments;
 import in.ac.iitm.students.complaint_box.objects.h_Complaint;
 import in.ac.iitm.students.others.MySingleton;
@@ -46,20 +47,18 @@ public class g_ComplaintAdapter extends RecyclerView.Adapter<g_ComplaintAdapter.
     private Activity activity;
     private Context context;
     private SharedPreferences sharedPref;
-    private boolean latest = false;
     private TextView tv_upvote;
     private TextView tv_downvote;
     private Button bn_upvote;
     private Button bn_downvote;
-    private Button bn_resolve;
 
 
-    public g_ComplaintAdapter(ArrayList<h_Complaint> myDataset, Activity a, Context c, Boolean latest) {
+
+    public g_ComplaintAdapter(ArrayList<h_Complaint> myDataset, Activity a, Context c) {
         mDataset = myDataset;
         activity = a;
         context = c;
         sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
-        this.latest = latest;
 
     }
 
@@ -68,13 +67,10 @@ public class g_ComplaintAdapter extends RecyclerView.Adapter<g_ComplaintAdapter.
                                                             int viewType) {
 
         View v;
-        if (latest) {
+
             v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.hostel_complaints_latest_complaint_card, parent, false);
-        }else {
-            v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.hostel_complaints_my_complaint_card, parent, false);
-        }
+                    .inflate(R.layout.g_complaint_card, parent, false);
+
 
         g_ComplaintAdapter.ViewHolder vh = new g_ComplaintAdapter.ViewHolder(v);
         return vh;
@@ -89,16 +85,14 @@ public class g_ComplaintAdapter extends RecyclerView.Adapter<g_ComplaintAdapter.
         TextView tv_title = (TextView) holder.view.findViewById(R.id.tv_title);
         TextView tv_tags = (TextView) holder.view.findViewById(R.id.tv_tags);
         TextView tv_description = (TextView) holder.view.findViewById(R.id.tv_description);
-        if (latest) tv_upvote = (TextView) holder.view.findViewById(R.id.tv_upvote);
-        if (latest) tv_downvote = (TextView) holder.view.findViewById(R.id.tv_downvote);
+         tv_upvote = (TextView) holder.view.findViewById(R.id.tv_upvote);
+         tv_downvote = (TextView) holder.view.findViewById(R.id.tv_downvote);
         TextView tv_comment = (TextView) holder.view.findViewById(R.id.tv_comment);
-        if (latest) bn_upvote = (Button) holder.view.findViewById(R.id.bn_upvote);
-        if (latest) bn_downvote = (Button) holder.view.findViewById(R.id.bn_downvote);
+         bn_upvote = (Button) holder.view.findViewById(R.id.bn_upvote);
+         bn_downvote = (Button) holder.view.findViewById(R.id.bn_downvote);
         Button bn_comment = (Button) holder.view.findViewById(R.id.bn_comment);
         ImageView iv_profile = (ImageView) holder.view.findViewById(R.id.imgProfilePicture);
         LinearLayout linearLayout = (LinearLayout) holder.view.findViewById(R.id.ll_comment);
-        final ImageButton bn_more_rooms = (ImageButton)holder.view.findViewById(R.id.more_rooms);
-        if(!latest) bn_resolve = (Button)holder.view.findViewById(R.id.bn_resolve);
 
 
         final h_Complaint hComplaint = mDataset.get(position);
@@ -109,11 +103,12 @@ public class g_ComplaintAdapter extends RecyclerView.Adapter<g_ComplaintAdapter.
         tv_resolved.setText(hComplaint.isResolved() ? "Resolved" : "Unresolved");
         tv_title.setText(hComplaint.getTitle());
         tv_description.setText(hComplaint.getDescription());
-        if (latest) tv_upvote.setText("" + hComplaint.getUpvotes());
-        if (latest) tv_downvote.setText("" + hComplaint.getDownvotes());
+        tv_upvote.setText("" + hComplaint.getUpvotes());
+        tv_downvote.setText("" + hComplaint.getDownvotes());
         tv_comment.setText("" + hComplaint.getComments());
-        if (hComplaint.getTag() != null && hComplaint.getTag().equals("")) tv_tags.setVisibility(View.INVISIBLE);
-        else tv_tags.setText(hComplaint.getTag());
+        //if (hComplaint.getTag() != null && hComplaint.getTag().equals("")) tv_tags.setVisibility(View.INVISIBLE);
+        //else
+        tv_tags.setText(hComplaint.getTag());
 
         //todo use glide and get profile picture
         if (hComplaint.getName() != null && hComplaint.getName().equals("Institute MobOps")) {
@@ -126,14 +121,14 @@ public class g_ComplaintAdapter extends RecyclerView.Adapter<g_ComplaintAdapter.
         if (hComplaint.isResolved()) {
             linearLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.resolved_colour));
 
-            if (latest) bn_upvote.setClickable(false);
-            if (latest) bn_downvote.setClickable(false);
+             bn_upvote.setClickable(false);
+             bn_downvote.setClickable(false);
             bn_comment.setClickable(false);
 
         } else {
             linearLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.unresolved_colour));
 
-            if (latest) bn_upvote.setOnClickListener(new View.OnClickListener() {
+            bn_upvote.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View view) {
@@ -191,7 +186,7 @@ public class g_ComplaintAdapter extends RecyclerView.Adapter<g_ComplaintAdapter.
                 }
             });
 
-            if (latest) bn_downvote.setOnClickListener(new View.OnClickListener() {
+             bn_downvote.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View view) {
@@ -253,80 +248,9 @@ public class g_ComplaintAdapter extends RecyclerView.Adapter<g_ComplaintAdapter.
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, h_Comments.class);
+                Intent intent = new Intent(context, g_Comments.class);
                 intent.putExtra("cardData", hComplaint);
                 activity.startActivity(intent);
-            }
-        });
-
-        bn_more_rooms.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //creating a popup menu
-                PopupMenu popup = new PopupMenu(context, bn_more_rooms);
-
-                MenuInflater inflater = popup.getMenuInflater();
-
-
-                String[] roomNumber = hComplaint.getMoreRooms().split(",");
-
-                for (String s:roomNumber) {
-                    //adding items to menu
-                    popup.getMenu().add(Menu.NONE,Menu.NONE,Menu.NONE,s);
-
-                }
-                //inflating popup menu from xml resource
-                inflater.inflate(R.menu.more_rooms_popup, popup.getMenu());
-                popup.show();
-            }
-        });
-
-        if(!latest) bn_resolve.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //if custom hComplaint
-                String url = "https://rockstarharshitha.000webhostapp.com/general_complaints/resolve.php";
-                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsObject = new JSONObject(response);
-                            if (jsObject.has("error")) {
-                                Toast.makeText(activity, jsObject.getString("error"), Toast.LENGTH_SHORT).show();
-                            } else if (jsObject.has("status")) {
-                                String status = jsObject.getString("status");
-                                if (status == "1") {
-                                    notifyItemChanged(position);
-                                } else {
-                                    Toast.makeText(activity, jsObject.getString("error"), Toast.LENGTH_SHORT).show();
-                                }
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }) {
-                    //to POST params
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<String, String>();
-                        //get hostel from prefs
-                        //put some dummy for now
-                        params.put("HOSTEL", "narmada");
-                        params.put("UUID", mUUID);
-                        return params;
-                    }
-
-                };
-                MySingleton.getInstance(activity).addToRequestQueue(request);
-
             }
         });
 
