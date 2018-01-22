b1eb126cc051765cb3d4e07309d8dcd2e4d41a39
sam10795 <silversoulsam@gmail.com>
2018-01-22 22:12:00 +0530

    Design changes

--- app/src/main/java/in/ac/iitm/students/adapters/EWAdapter.java
+++ app/src/main/java/in/ac/iitm/students/adapters/EWAdapter.java
@@ -7,7 +7,6 @@ import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +19,6 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.hdodenhof.circleimageview.CircleImageView;
 import in.ac.iitm.students.R;
 import in.ac.iitm.students.objects.ew_member;
 
@@ -47,7 +45,7 @@ public class EWAdapter extends ArrayAdapter {
         if(convertView==null)
         {
             LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.item_ew_member,parent,false);
+            convertView = inflater.inflate(R.layout.item_ew_member, parent, false);
             holder = new ViewHolder();
             holder.tv_name = (TextView) convertView.findViewById(R.id.ew_name);
             holder.tv_title = (TextView) convertView.findViewById(R.id.ew_title);
