e51f6467df678233798691a5bba6a2414d63b6c5
sam10795 <silversoulsam@gmail.com>
2018-02-12 21:25:28 +0530

    Updated Subscriptions

---
+++ app/src/main/java/in/ac/iitm/students/fragments/MyComplaintsFragment.java
@@ -0,0 +1,27 @@
+package in.ac.iitm.students.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sam10795 on 11/2/18.
+ */
+
+public class MyComplaintsFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
